2692c02abf5f7af2a57edb84c8428080d34ac5ec
Clement127001 <babiyonclement18@gmail.com>
2024-02-18 23:34:28 +0530

    add the details page

---
+++ assets/js/global.js
@@ -0,0 +1,16 @@
+"use-strict";
+
+//adding the event to mutiple elements
+function addEventOnElements(elements, callback) {
+  elements.forEach(function (elem) {
+    elem.addEventListener("click", callback);
+  });
+}
+
+//toggling the search box
+const searchBox = document.querySelector(".search-box");
+const searchTogglers = document.querySelectorAll("[search-toogler]");
+
+addEventOnElements(searchTogglers, function () {
+  searchBox.classList.toggle("active");
+});
